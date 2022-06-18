c930c78129814a769a6a481a625fb7f88a08d468
kodyazar <kodyazar@gmail.com>
2022-06-18 15:32:23 +0300

    virgül eklendi

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Merhaba DORUK!</Text>
+      <Text>Merhaba, DORUK!</Text>
       <StatusBar style="auto" />
     </View>
   );
