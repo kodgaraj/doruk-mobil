1f6a66c4d7e67592f97d3134785ea93766e0dd70
Cihat <chtsngn61@gmail.com>
2022-06-23 21:18:27 +0300

    Hata düzeltmesi

--- src/screens/Islemler.jsx
+++ src/screens/Islemler.jsx
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
-import { useLoading } from "../utils/LoadingContext";
 
 export default function Islemler() {
   return (
     <View>
-      <Text>{loading.toString()}</Text>
+      <Text>İşlemler</Text>
     </View>
   )
 }
