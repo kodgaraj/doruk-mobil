27fe52285c313dbf1c717eee4af7a0447a2e963b
Cihat <chtsngn61@gmail.com>
2022-06-27 12:03:19 +0300

    Düzeltme

--- src/screens/Siparisler.jsx
+++ src/screens/Siparisler.jsx
@@ -1,16 +1,19 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
+import { StackActions } from "@react-navigation/native";
 
 export default function Siparisler({ navigation }) {
+  const handleDetails = () => {
+    const pushAction = StackActions.push("Home", { user: "Wojtek" });
+    navigation.dispatch(pushAction);
+  };
+
   return (
     <View>
       <Text>Siparişler</Text>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push("Home")}
-        />
+        <Button title="Go to Details... again" onPress={handeDetails} />
       </View>
     </View>
   );
