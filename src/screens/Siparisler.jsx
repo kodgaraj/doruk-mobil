55712ece591f21b20a75ab96c9b5e990067e5421
Cihat <chtsngn61@gmail.com>
2022-06-27 11:49:04 +0300

    Detay sayfası örneği

--- src/screens/Siparisler.jsx
+++ src/screens/Siparisler.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
 
-export default function Siparisler() {
+export default function Siparisler({ navigation }) {
   return (
     <View>
       <Text>Siparişler</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push("Home")}
+        />
+      </View>
     </View>
-  )
+  );
 }
