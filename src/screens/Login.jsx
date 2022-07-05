24a3b36a0aebef6bd9b0a2b5fdaecfdb6a5de503
kodyazar <kodyazar@gmail.com>
2022-07-05 10:14:39 +0300

    Login input klavye sorunu
    KeyboardAvoidingView kullanılarak klavye input sorunu çözüldü.

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { View, KeyboardAvoidingView, Platform,TouchableWithoutFeedback, Keyboard, StyleSheet, ImageBackground } from "react-native";
 import {
   Avatar,
   Button,
@@ -55,68 +55,72 @@ function Login() {
   };
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={{
-          uri: "https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMTg3OTJ8MHwxfGNvbGxlY3Rpb258NXw4NGNVeS0wS1pTUXx8fHx8Mnx8MTY1NTc5ODQ3MQ&ixlib=rb-1.2.1&q=80&w=1080",
-        }}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <Card elevation={5} style={styles.card}>
-          <View style={styles.coverContainer}>
-            <ImageBackground
-              source={{
-                uri: "https://doruk.kodgaraj.com/img/doruk-logo.png",
-              }}
-              resizeMode="contain"
-              style={styles.cover}
-            />
-          </View>
-          <Card.Content style={styles.row}>
-            <Title>Giriş Yap</Title>
-          </Card.Content>
-          <Card.Content style={styles.column}>
-            <View style={styles.input}>
-              <TextInput
-                label="E-posta"
-                value={email}
-                dense
-                onChangeText={(text) => {
-                  setEmail(text);
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImageBackground
+          source={{
+            uri: "https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMTg3OTJ8MHwxfGNvbGxlY3Rpb258NXw4NGNVeS0wS1pTUXx8fHx8Mnx8MTY1NTc5ODQ3MQ&ixlib=rb-1.2.1&q=80&w=1080",
+          }}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <Card elevation={5} style={styles.card}>
+            <View style={styles.coverContainer}>
+              <ImageBackground
+                source={{
+                  uri: "https://doruk.kodgaraj.com/img/doruk-logo.png",
                 }}
-              ></TextInput>
+                resizeMode="contain"
+                style={styles.cover}
+              />
             </View>
-            <View style={styles.input}>
-              <TextInput
-                label="Şifre"
-                value={password}
-                dense
-                secureTextEntry={!showPassword}
-                onChangeText={(text) => {
-                  setPassword(text);
-                }}
-                right={
-                  password && (
-                    <TextInput.Icon
-                      name={!showPassword ? "eye" : "eye-off"}
-                      onPress={() => {
-                        setShowPassword(!showPassword);
-                      }}
-                    />
-                  )
-                }
-              ></TextInput>
-            </View>
-          </Card.Content>
-          <Card.Actions style={styles.row}>
-            <Button mode="contained" onPress={handleLogin}>
-              GİRİŞ YAP
-            </Button>
-          </Card.Actions>
-        </Card>
-      </ImageBackground>
-    </View>
+            <Card.Content style={styles.row}>
+              <Title>Giriş Yap</Title>
+            </Card.Content>
+            <Card.Content style={styles.column}>
+              <View style={styles.input}>
+                <TextInput
+                  label="E-posta"
+                  value={email}
+                  dense
+                  onChangeText={(text) => {
+                    setEmail(text);
+                  }}
+                ></TextInput>
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  label="Şifre"
+                  value={password}
+                  dense
+                  secureTextEntry={!showPassword}
+                  onChangeText={(text) => {
+                    setPassword(text);
+                  }}
+                  right={
+                    password && (
+                      <TextInput.Icon
+                        name={!showPassword ? "eye" : "eye-off"}
+                        onPress={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    )
+                  }
+                ></TextInput>
+              </View>
+            </Card.Content>
+            <Card.Actions style={styles.row}>
+              <Button mode="contained" onPress={handleLogin}>
+                GİRİŞ YAP
+              </Button>
+            </Card.Actions>
+          </Card>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
